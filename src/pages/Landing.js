8f367dd4784de9b27a68b05a2d7bbb332b1c4cd0
Steve <steve0183@gmail.com>
2021-03-31 08:15:58 -0600

    add login form and create new account button to landing component

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,54 @@
-import React from "react";
-
-export default function LandingPage() {
-  return (
-    <div>
-      <h1>Landing Page</h1>
-      <h2>Welcome to the BookLists App</h2>
-    </div>
-  );
+const LandingPage = () => {
+	return (
+		<div className="">
+			<div className="">LOGO</div>
+			<h1 className="">Track Your Reading</h1>
+			<div className="">
+				<span className=""></span>
+				<p className="">Already have an account? Sign in below</p>
+				<span className=""></span>
+			</div>
+			<div>
+				<form action=''>
+					<div className=''>
+						<label for='email' className=''>
+							Email Address
+						</label>
+						<input
+							type='email'
+							id='email'
+							name='user_email'
+							placeholder='john@email.com'
+							className=''
+						/>
+					</div>
+					<div className=''>
+						<label for='password' className=''>
+							Password
+						</label>
+						<input
+							type='password'
+							id='password'
+							name='user_password'
+							className=''
+						/>
+					</div>
+					<div>
+						<input
+							type='checkbox'
+							name='remember'
+							id='remember_check'
+							className=''
+						/>
+						<span className=''>Forgot your password?</span>
+					</div>
+					<button type='submit' className=''>
+						Sign in
+					</button>
+				</form>
+			</div>
+		</div>
+	)
 }
+
+export default LandingPage
