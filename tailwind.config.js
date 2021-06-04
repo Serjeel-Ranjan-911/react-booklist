97114e30adb9fa6ead7a1f83252e0a982f3bbc1e
Steven Saunders <steve0183@gmail.com>
2021-06-03 20:06:15 -0600

    accepted remote changes from merge conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			colors: {
-				lightblue: {
-					100: '#aceaff',
-					800: '#205e82',
-					900: '#0C4A6E'
-				},
-				cyan: {
-					900: '#164E63'
-				}
-			},
-			fontFamily: {
-				inter: ['Inter', 'sans-serif'],
-			},
-		},
-	},
-	variants: {
-		extend: {
-			backgroundColor: ['active'],
-		},
-	},
-	plugins: [],
-}
-=======
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -58,4 +30,3 @@ module.exports = {
   },
   plugins: [],
 };
->>>>>>> main
